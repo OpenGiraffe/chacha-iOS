028e21e82d286c8f1a63ad2ebf0f57a201c682b0
1024 <1024@2miao.cn>
2020-01-08 19:23:17 +0800

    RtcCell

---
+++ chacha-iOS/Client/Chat/Cell/LLMessageRtcCell.h
@@ -0,0 +1,17 @@
+//
+//  LLMessageTextCell.h
+//  LLWeChat
+//
+//  Created by GYJZH on 7/21/16.
+//  Copyright © 2016 GYJZH. All rights reserved.
+//
+
+#import "LLMessageBaseCell.h"
+#import "LLMessageVoiceCell.h"
+#import "LLMessageModel.h"
+
+@interface LLMessageRtcCell : LLMessageVoiceCell
+
++ (UIFont *)font;
+
+@end
